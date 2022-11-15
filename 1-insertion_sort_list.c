44c9801eca30026f63391c09bb26ab2b19e18fd1
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-11-15 18:29:43 +0100

    betty style

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * insertion_sort_list - sort a doubly linked list for all integers
  * in ascending order using the insert sort
@@ -10,9 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp, *node, *temp, *temp1, *temp2;
 
-	if (list == NULL)
-		return;
-	if ((*list)->next == NULL)
+	if (list == NULL || (*list)->next == NULL)
 		return;
 	tmp = (*list)->next;
 	while (tmp)
@@ -24,9 +21,7 @@ void insertion_sort_list(listint_t **list)
 			temp1 = node->prev->prev;
 			if (temp1 != NULL)
 				temp1->next = temp;
-			temp2 = node->next;
-			/*if (temp2 == NULL)
-				break;*/
+			temp2 = node->next; /*if (temp2 == NULL) break;*/
 			node->prev->prev = temp;
 			node->prev->next = temp2;
 			if (node->next != NULL)
@@ -35,37 +30,6 @@ void insertion_sort_list(listint_t **list)
 			node->prev = temp1;
 			if (node->prev == NULL)
 				*list = node;
-
-
-
-
-
-			/*temp = node;
-			temp1 = node->prev;
-			temp2 = node->prev->prev;
-			temp3 = node->next;
-			if (temp2 != NULL)
-				temp2->next = temp;
-			else
-				*list = temp;
-			temp1->next = temp3;
-			temp1->prev = temp;
-			node->next = temp1;
-			node->prev = temp2;
-			if (node->next != NULL)
-				node->next->prev = temp1;*/
-/*
-
-
-
-			if (node->prev->prev == NULL)
-				*list = temp;
-			node->prev->prev->next = temp;
-			node->prev->prev = temp;
-			node->prev = temp2;
-			node->next = temp1;
-			node->next->prev = temp1;
-			node->prev->next = temp3;*/
 			print_list(*list);
 			if (node->prev == NULL)
 				break;
