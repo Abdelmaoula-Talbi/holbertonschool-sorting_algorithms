b91841ebb892200b2f15dcc78148ae55fcf814fc
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-11-15 18:22:20 +0100

    fix code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp, *node, *temp1, *temp2;
+	listint_t *tmp, *node, *temp, *temp1, *temp2;
 
 	if (list == NULL)
 		return;
@@ -18,21 +18,57 @@ void insertion_sort_list(listint_t **list)
 	while (tmp)
 	{
 		node = tmp;
-		while (node != NULL && node->n < node->prev->n)
+		while (node->n < node->prev->n)
 		{
-			temp1 = node->prev;
-			if (temp1->prev == NULL)
-				node = *list;
+			temp = node;
+			temp1 = node->prev->prev;
+			if (temp1 != NULL)
+				temp1->next = temp;
 			temp2 = node->next;
-			if (temp2 == NULL)
-				break;
-			node->prev->prev->next = node;
+			/*if (temp2 == NULL)
+				break;*/
+			node->prev->prev = temp;
 			node->prev->next = temp2;
-			node->prev->prev = node;
-			node->next->prev = temp1;
+			if (node->next != NULL)
+				node->next->prev = node->prev;
 			node->next = node->prev;
-			node->prev = node->prev->prev;
+			node->prev = temp1;
+			if (node->prev == NULL)
+				*list = node;
+
+
+
+
+
+			/*temp = node;
+			temp1 = node->prev;
+			temp2 = node->prev->prev;
+			temp3 = node->next;
+			if (temp2 != NULL)
+				temp2->next = temp;
+			else
+				*list = temp;
+			temp1->next = temp3;
+			temp1->prev = temp;
+			node->next = temp1;
+			node->prev = temp2;
+			if (node->next != NULL)
+				node->next->prev = temp1;*/
+/*
+
+
+
+			if (node->prev->prev == NULL)
+				*list = temp;
+			node->prev->prev->next = temp;
+			node->prev->prev = temp;
+			node->prev = temp2;
+			node->next = temp1;
+			node->next->prev = temp1;
+			node->prev->next = temp3;*/
 			print_list(*list);
+			if (node->prev == NULL)
+				break;
 		}
 		tmp = tmp->next;
 	}
